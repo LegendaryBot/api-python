ee1963af029334aa9c9c378cb0b53c8c959e159b
William Lebel <lebelwilliam@gmail.com>
2018-05-28 01:05:07 -0400

    Added LegionBuiding & Overwatch unit test. Added Codacy support

---
+++ tests/test_overwatch.py
@@ -0,0 +1,37 @@
+import overwatch.overwatch as ow
+
+
+def test_overwatch():
+    event = {
+        "pathParameters": {
+            "battletag": "Greatman-1189",
+            "region": "us"
+        }
+    }
+    result = ow.get_overwatch_stats(event, None)
+    assert result['statusCode'] == 404
+    event = {
+        "pathParameters": {
+            "battletag": "Spek-11212",
+            "region": "us"
+        }
+    }
+    result = ow.get_overwatch_stats(event, None)
+    assert result['statusCode'] == 200
+
+    event = {
+        "pathParameters": {
+            "battletag": "Spek-11212",
+            "region": "asdf"
+        }
+    }
+    result = ow.get_overwatch_stats(event, None)
+    assert result['statusCode'] == 404
+    event = {
+        "pathParameters": {
+            "battletag": "Spek-112123",
+            "region": "us"
+        }
+    }
+    result = ow.get_overwatch_stats(event, None)
+    assert result['statusCode'] == 404
--- tests/test_wow_region.py
+++ tests/test_wow_region.py
@@ -30,3 +30,20 @@ def test_wow_region_get_token_price():
     }
     result = wr.get_token_price(event, None)
     assert result['statusCode'] == 404
+
+def test_wow_region_get_legion_building():
+    event = {
+        "pathParameters": {
+            "region": "us"
+        }
+    }
+    result_us = wr.get_legion_building(event, None)
+    event = {
+        "pathParameters": {
+            "region": "eu"
+        }
+    }
+    result_eu = wr.get_legion_building(event, None)
+    assert result_us != result_eu
+    assert result_us['statusCode'] == 200
+    assert result_eu['statusCode'] == 200
